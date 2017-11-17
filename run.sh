df748d4892f8f1ab58a38239a3e33f2e3e880d5d
Dan Taeyoung <dan@dantaeyoung.com>
2017-11-17 18:24:53 +0000

    documentation of run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+# disable autofocus and set focus manually 
 v4l2-ctl -c focus_auto=0
 v4l2-ctl -c focus_absolute=20
-amixer cset numid=3 1
+
+# switch output to headphone jack
+amixer cset numid=3 1 
+
+# set volume to 100$
 amixer sset PCM,0 100%
+
+# set display
 export DISPLAY=:0
+
 python3 main.py
